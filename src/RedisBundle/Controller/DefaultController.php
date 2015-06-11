3ec3b3da9377dc3cf79a9b5293fc424e3b6bad41
joan.cruz <joan.cruz@atrapalo.com>
2015-06-11 13:35:44 +0200

    rm idea

--- src/RedisBundle/Controller/DefaultController.php
+++ src/RedisBundle/Controller/DefaultController.php
@@ -9,18 +9,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('RedisBundle:Index:index.html.twig');
     }
 
 
     /**
-     * @Route("/", name="homepage")
+     * @Route("/ex1", name="exercice 1")
      */
-    public function homeAction()
+    public function ex1Action()
     {
         /** @var \Predis\Client $redisCli */
         $redisCli = new Client([
@@ -28,6 +28,7 @@ class DefaultController extends Controller
             'host'   => '127.0.0.1',
             'port'   => 6379,
         ]);
+
         $redisCli->set("foo", 2);
 
         echo $redisCli->get('foo');
